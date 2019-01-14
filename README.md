7de2a50e72e5fcb77836c24601062f27cf98ba7f
revathikrish <revathi9544@gmail.com>
2019-01-14 16:30:25 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# mzc-androidclass" 
---
+++ app/src/main/java/com/mzc/amal/mzcc/MzcActivity.java
@@ -0,0 +1,13 @@
+package com.mzc.amal.mzcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MzcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mzc);
+    }
+}
